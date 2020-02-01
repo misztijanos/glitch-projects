7bc241dbc884de7b860b4aa5808e4188973da69a
Glitch (simple-constructor-example) <none>
2020-02-01 14:54:03 +0000

    🎍🚕 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,52 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+
+function Person(first, last, age, gender, interests) {
+  this.name = {
+    first: first,
+    last: last
+  };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
+  this.bio = function() {
+    let pronoun =
+      this.gender === "male"
+        ? "He likes"
+        : this.gender === "female"
+        ? "She likes"
+        : "They like";
+    let hobbyString = this.interests
+      .map(function(currentValue, index) {
+        return index === 0
+          ? currentValue
+          : //within the callback function, `this` refers to `this.interests`, because I'm using the thisValue parameter
+          index !== this.length - 1
+          ? ", " + currentValue
+          : " and " + currentValue;
+      }, this.interests)
+      .join("");
+    alert(
+      this.name.first +
+        " " +
+        this.name.last +
+        " is " +
+        this.age +
+        " years old. " +
+        pronoun +
+        " " +
+        hobbyString +
+        "."
+    );
+  };
+}
+
+let person1 = new Person("Bobby", "Smith", 32, "female", [
+  "music",
+  "skiing",
+  "weed"
+]);
+person1.bio();
